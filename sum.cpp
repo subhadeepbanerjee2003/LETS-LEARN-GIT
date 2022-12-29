f68fe309a5cb733209845ebe70a01717a7f153de
Subhadeep Banerjee <banerjeesubhodeep444@gmail.com>
2022-12-30 00:28:00 +0530

    added message in sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-int a,b;
-cout<<"enter two number"<<endl;
-cin>>a>>b;
-cout<<(a+b)<<endl;
-return 0;
+ int a,b;
+ cout<<"enter two number"<<endl;
+ cin>>a>>b;
+ cout<<"the sum is:"<<(a+b)<<endl;
+ return 0;
 }
